5fdde1dc8caea0d461204fd9fc60fb64ec910844
DQ_Yang <dqshop@dq-yangdeimac.local>
2017-12-15 11:12:21 +0800

    Initial Commit

---
+++ MTShop/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MTShop
+//
+//  Created by DQ_Yang on 2017/12/15.
+//  Copyright © 2017年 MT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MTShop/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MTShop
+//
+//  Created by DQ_Yang on 2017/12/15.
+//  Copyright © 2017年 MT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
